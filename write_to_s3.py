900c5b06f3828dd9e055976c37726e75a97410df
Brent <blemieux4@gmail.com>
2017-05-16 15:19:02 -0500

    changes to make scripts more reusable

--- write_to_s3.py
+++ write_to_s3.py
@@ -1,8 +1,10 @@
+'''
+This script takes 3 additional arguments and runs like so:
+    python write_to_s3.py file_name bucket_name path_to_write
+'''
+from sys import argv
+
 import boto3
-
-# Specify the service
-s3 = boto3.resource('s3')
-
 
 def to_bucket(f, bucket, write_path):
     '''
@@ -10,8 +12,15 @@ def to_bucket(f, bucket, write_path):
 
     INPUT: f - file to write
            bucket - bucket to write to
-           write_path - path in bucket to write file 
+           write_path - path in bucket to write file
     '''
+    # Specify the service
+    s3 = boto3.resource('s3')
     data = open(f, 'rb')
     s3.Bucket(my_bucket).put_object(Key='{}/{}'.format(write_path, f), Body=data)
     print('Success! {} added to {} bucket @ {}'.format(f, bucket, write_path))
+
+
+if __name__ == '__main__':
+    _, f, bucket_name, write_path = argv
+    to_bucket(f, bucket_name, write_path)
